5840da961740e3729f1070f5f54771497fe2aa84
Gijsbert-Pr <g.j.praamstra@student.rug.nl>
2024-09-19 11:30:01 +0200

    update

--- r-scripts/10-multivariate analysis.R
+++ r-scripts/10-multivariate analysis.R
@@ -197,24 +197,37 @@ vegan::ordisurf(cca2,envdat$DistGulley_m, add=T, col="green")
 
 ##### cluster analysis (classification) of  communities
 # first calculate a dissimilarity matrix, using Bray-Curtis dissimilarity
-
+d<-vegan::vegdist(vegdat, method="bray") # calculate Bray-Curtis dissimilarity matrix)
 
  # show the dissimilarity matrix (1= completely different, 0= exactly the same)
-
+d
 
 # now cluster the sites based on similarity in species composition 
 # using average linkage as the sorting algorithm
-
+cavg<-hclust(d, method="average") # calculate the average linkage clustering
+plot(cavg)
 
 # back to  clustering based on species composition - show the dendrogram and cut in in 4 communities
-
+rect.hclust(cavg, k=4) # cut the dendrogram in 4 clusters
+c4<-cutree(cavg, k=4) # assign the plots to the 4 clusters
+c4
 
 ##### add the clustering of plots to your cca ordination
-
+vegan::ordiplot(cca1, display="sites", cex=1, type="text", xlab="CCA1 (21%)", ylab="CCA2 (14%)")
+vegan::orditorp(cca1, display="species", priority=SpecTotCov, col="red", pcol="red", pch="+", cex=1.1)
+vegan::ordihull(cca1,c4, add=T, lty=2, col="darkgreen", lwd=2)
 #add the vegetation type to the environmental data
-
+envdat2<-envdat|>
+  dplyr::mutate(vegtype=factor(c4)) # add the vegetation type to the environmental data
+levels(envdat2$vegtype)<-c("Dune","High Saltmarch","Low Saltmarsh","Pioneer zone") # rename the levels of the factor
 # test if DistGulley_m is different between the vegetation types
-
+p1<-envdat2 |>
+  ggplot(aes(x=vegtype, y=floodprob)) +
+  geom_boxplot()
+p2<-envdat2 |>
+  ggplot(aes(x=vegtype, y=clay_cm)) +
+  geom_boxplot()
+p1+p2+patchwork::plot_layout(ncol=1)
 # what do you write: 
 # the vegetation types were significantly different in distance to gulley (F3,18=21.36, P<0.001)
 # * P<0.05, ** P<0.01, *** P<0.001
